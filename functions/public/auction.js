6eff2b03e8cc678f7043682da8346a2eebd5b9fe
ballsri <ball04407@hotmail.com>
2022-05-08 20:03:00 +0700

    Biddee tenth commit

--- functions/public/auction.js
+++ functions/public/auction.js
@@ -1,21 +1,21 @@
 
 var form = document.getElementById('search')
-if(form != null){
+if (form != null) {
   form.addEventListener('submit', search);
 
 }
-function search(event){
+function search(event) {
   event.preventDefault();
   var find = document.getElementById('search__text').value;
   function containsSpecialChars(str) {
     const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
     return specialChars.test(str);
   }
-  if(containsSpecialChars(find)){
+  if (containsSpecialChars(find)) {
     alert('Special Character are not allowed!')
-  } else{
+  } else {
 
-    window.location.href ='/auction/searched-result?q='+find;
+    window.location.href = '/auction/searched-result?q=' + find;
   }
 
 }
@@ -26,52 +26,57 @@ function search(event){
 
 
 function animateValue(item, start = 0) {
-    
-  console.log(item);  
-    var time = item.innerHTML;
-// console.log(time);
-  
-var countDownDate = new Date(time).getTime();
-   
-    var x = setInterval(function() {
 
-        // Get today's date and time
-        var now = new Date();
-//	    console.log(now);
-	    now = now.getTime();
-	    
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-    
-        // Time calculations for days, hours, minutes and seconds
-    
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));  
-        // Output the result in an element with id="demo"
-        if(days >0){
-        item.innerHTML =  "~ "+ (days+1) +" days";
-        } else {
-    
-        item.innerHTML =  hours + "h "
-        + minutes + "m " + seconds + "s ";
-        }
-    
-        // If the count down is over, write some text 
-        if (distance < 0) {
-            clearInterval(x);
-            document.getElementById("time").innerHTML = "EXPIRED";
-            var btn = document.getElementById('bidbt');
-            btn.remove();
-        }
-        }, 1000);
-  }
+  if(item == null) return;
+
+  var time, countDownDate;
+    time = item.innerHTML;
+
+    countDownDate = new Date(time);
+    countDownDate= countDownDate.getTime();
+
   
-  document.querySelectorAll('.Ticking').forEach((item)=>{
-//	console.log(item);
-//    animateValue(item);
-  });
+  
+  var x = setInterval(function () {
+    
+    // Get today's date and time
+    var now = new Date();
+    //	    console.log(now);
+    now = now.getTime();
+    
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+    
+    // Time calculations for days, hours, minutes and seconds
+    
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    // If the count down is over, write some text 
+    if (distance <= 0) {
+      item.innerHTML = "EXPIRED";
+      clearInterval(x);
+      return;
+    }
+    // Output the result in an element with id="demo"
+    if (days > 0) {
+      item.innerHTML = "~ " + (days + 1) + " days";
+    } else {
+      
+      item.innerHTML = hours + "h "
+      + minutes + "m " + seconds + "s ";
+    }
+    
+    
+    
+  }, 1000);
+}
+
+document.querySelectorAll('.Ticking').forEach((item) => {
+  //	console.log(item);
+  animateValue(item);
+});
 
 
 
@@ -82,5 +87,5 @@ var countDownDate = new Date(time).getTime();
 
 const page = document.getElementById('page');
 setTimeout(() => {
-page.style.visibility = 'visible';
+  page.style.visibility = 'visible';
 }, 1000); // 
